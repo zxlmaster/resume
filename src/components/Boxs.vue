f56a9a5ccf5d6b81e231a0a9bc278c0d71d1e729
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-09 23:40:08 +0800

    [组件] 完成 Boxs 组件 用于生成相关内容

---
+++ src/components/Boxs.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h2 class="btn subtitle"> {{ title }} </h2>
+        <box v-for="item in list"
+          :key="item.name"
+          :content="item"></box>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Box from '@/components/Box';
+
+export default {
+  name: 'Boxs',
+  props: {
+    list: Array,
+    title: String,
+  },
+  components: {
+    box: Box,
+  },
+};
+</script>
+
+<style scoped>
+</style>
