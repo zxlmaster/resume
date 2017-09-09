00fa3c29b0797579ffd32fcea3d05fe42f55e28b
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-09 23:41:56 +0800

    [组件] 完成 Resume 组件 用于展示用户简历

---
+++ src/components/Resume.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="resume">
+    <personal :user="user"></personal>
+    <boxs :title="experienceTitle" :list="experience"></boxs>
+    <boxs :title="skillTitle" :list="experience"></boxs>
+  </div>
+</template>
+
+<script>
+import Personal from '@/components/Personal';
+import Boxs from '@/components/Boxs';
+import user from '@/database/personal';
+import experience from '@/database/experience';
+
+export default {
+  components: {
+    personal: Personal,
+    boxs: Boxs,
+  },
+  data() {
+    return {
+      user,
+      experience,
+      experienceTitle: 'Experiences',
+      skillTitle: 'Skills',
+    };
+  },
+};
+</script>
