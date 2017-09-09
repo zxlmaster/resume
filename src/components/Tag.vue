6c3f5669b41074b9fa0370b1eb7049cf25063aff
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-09 23:42:31 +0800

    [组件] 完成 Tag 组件 用于生成单个 tag 组件，可以选择组件颜色

---
+++ src/components/Tag.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="control">
+    <div class="tags has-addons">
+      <span class="tag">
+        <icon v-if="icon" size="small" :icon="icon"></icon>
+        <span v-else>{{ title }}</span>
+      </span>
+      <span :class="contentClass">{{ content }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon';
+
+export default {
+  name: 'SocialTags',
+  props: {
+    title: String,
+    icon: String,
+    content: String,
+    colour: String,
+  },
+  components: {
+    icon: Icon,
+  },
+  computed: {
+    contentClass() {
+      return this.colour
+        ? `tag is-${this.colour}`
+        : 'tag is-info';
+    },
+  },
+};
+</script>
