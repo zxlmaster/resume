0ad45100b2b3d28d8c251fba8479f3c8b9347e06
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-09 23:40:26 +0800

    [组件] 完成 Box 组件 用于生成相关内容条目

---
+++ src/components/Box.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="resume-box">
+    <h3>{{content.name}}</h3>
+    <p v-if="showTime" class="time">{{ time }}</p>
+    <ul v-for="desc in content.description">
+      <li>{{ desc }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Box',
+  props: {
+    content: Object,
+  },
+  computed: {
+    showTime() {
+      return this.content.time !== undefined;
+    },
+    time() {
+      const start = this.content.time.start;
+      const end = this.content.time.end;
+
+      return `${start} 至 ${end}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .resume-box {
+    margin-bottom: 25px;
+  }
+  .resume-box li {
+    color: #999;
+  }
+</style>
