dc08bc72be5a8584777e12da15ab3400ca6d0b1d
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-09 23:41:40 +0800

    [组件] 完成 Personal 组件 用于展示用户相关信息

---
+++ src/components/Personal.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{ user.username }}</h1>
+        <h2 class="subtitle">{{ user.profession }}</h2>
+        <tags :tags="user.school" colour="success"></tags>
+        <tags :tags="user.social"></tags>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Tags from '@/components/Tags';
+
+export default {
+  name: 'Personal',
+  props: {
+    user: Object,
+  },
+  components: {
+    tags: Tags,
+  },
+};
+</script>
