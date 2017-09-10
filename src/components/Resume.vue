0235e9fb2f17d2186c71117da319ea3967725147
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-10 23:22:11 +0800

    [组件] Resume 组件中 增加 代码、文章、技能 的 Boxs 组件使用

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,7 +2,9 @@
   <div class="resume">
     <personal :user="user"></personal>
     <boxs :title="experienceTitle" :list="experience"></boxs>
-    <boxs :title="skillTitle" :list="experience"></boxs>
+    <boxs :title="skillTitle" :list="skill"></boxs>
+    <boxs :title="articleTitle" :list="article"></boxs>
+    <boxs :title="codeTitle" :list="code"></boxs>
   </div>
 </template>
 
@@ -11,6 +13,9 @@ import Personal from '@/components/Personal';
 import Boxs from '@/components/Boxs';
 import user from '@/database/personal';
 import experience from '@/database/experience';
+import skill from '@/database/skill';
+import article from '@/database/article';
+import code from '@/database/code';
 
 export default {
   components: {
@@ -20,9 +25,14 @@ export default {
   data() {
     return {
       user,
+      code,
+      skill,
+      article,
       experience,
+      codeTitle: 'Open Source',
       experienceTitle: 'Experiences',
       skillTitle: 'Skills',
+      articleTitle: 'Article',
     };
   },
 };
